d17a632e6758bf9241bbce321e656c5d9c07b9b2
August Wester <august.wester@gmail.com>
2020-04-14 19:01:10 +0200

    Another attempt

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
-    name='aux',
-    packages=['aux'],
- )
+setup(name='aux',
+      version='0.1',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
