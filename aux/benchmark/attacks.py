f02c452cf6a7a14e97b930159dafb8635f9c2c7a
Rasmus Elias Thisted <rasmus@Rasmuss-MacBook-Pro.local>
2020-04-15 20:16:31 +0200

    New revision

--- aux/benchmark/attacks.py
+++ aux/benchmark/attacks.py
@@ -5,13 +5,12 @@ import numpy as np
 
 
 def pgd_attack(model, images, labels):
-    """ Evaluates robustness against a PGD attack with random restart and 40 iterations.
+    """ Evaluates robustness against a PGD attack with random restart and 40 steps.
     Args:
         model : Tensorflow model to evaluate.
         images : Clean images that will be turned into adversarial examples
         labels : Labels of the clean images
     """
-
 
     # Preprocess test data to feed to Foolbox
     images, labels = tf.convert_to_tensor(images, dtype_hint=tf.float32), tf.convert_to_tensor(labels, dtype_hint=tf.int64)
@@ -33,26 +32,3 @@ def pgd_attack(model, images, labels):
     for i in range(len(epsilons)):
         num_successes = np.count_nonzero(success_[i]==1)
         print("For epsilon = {}, there were {}/{} successful attacks (robustness = {})".format(epsilons[i], num_successes, nb_attacks, round(1.0 - num_successes / nb_attacks, 2)))
-
-### JUST FOR TESTING ###
-def define_model():
-    model = tf.keras.Sequential([
-          tf.keras.layers.Flatten(input_shape=(28, 28)),
-          tf.keras.layers.Dense(128, activation='relu'),
-          tf.keras.layers.Dense(10)
-    ])
-  
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-    
-    model.fit(X_train, y_train, epochs=10)
-    return model
-
-# Download mnist data and split into train and test sets
-(X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
-
-# Preprocess data
-X_train, X_test = X_train/255, X_test/255
-
-pgd_attack(define_model(), x_test, y_test)
