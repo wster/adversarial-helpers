4b02e97c47c7783294e12b39bad4caa11fc8b120
August Wester <august.wester@gmail.com>
2020-04-15 23:43:17 +0200

    New revision

--- aux/datasets/cifar10.py
+++ aux/datasets/cifar10.py
@@ -6,9 +6,7 @@ def load_data():
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
     x_train = x_train.astype('float32') / 255
-    x_train = np.expand_dims(x_train, axis=-1)
     x_test = x_test.astype('float32') / 255
-    x_test = np.expand_dims(x_test, axis=-1)
 
     y_train = to_categorical(y_train)
     y_test = to_categorical(y_test)
--- build/lib/aux/datasets/noisy_cifar10.py
+++ build/lib/aux/datasets/noisy_cifar10.py
@@ -6,9 +6,7 @@ def load_data(low, high):
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
     x_train = x_train.astype('float32') / 255
-    x_train = np.expand_dims(x_train, axis=-1)
     x_test = x_test.astype('float32') / 255
-    x_test = np.expand_dims(x_test, axis=-1)
 
     noise_train = np.random.uniform(low=low, high=high, size=x_train.shape)
     noise_test = np.random.uniform(low=low, high=high, size=x_test.shape)
