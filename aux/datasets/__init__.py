341e93a05ec56b1fa5a96fe7b2f5549b07b24d48
August Wester <august.wester@gmail.com>
2020-04-17 13:52:44 +0200

    New revision

--- aux/datasets/__init__.py
+++ aux/datasets/__init__.py
@@ -1,4 +1,8 @@
 from .mnist import load_data
 from .noisy_mnist import load_data
+
 from .cifar10 import load_data
 from .noisy_cifar10 import load_data
+
+from .fmnist import load_data
+from .noisy_fmnist import load_data
---
+++ build/lib/aux/datasets/fmnist.py
@@ -0,0 +1,16 @@
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+
+def load_data():
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+    x_train = x_train.astype('float32') / 255
+    x_train = np.expand_dims(x_train, axis=-1)
+    x_test = x_test.astype('float32') / 255
+    x_test = np.expand_dims(x_test, axis=-1)
+
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+
+    return (x_train, y_train), (x_test, y_test)
