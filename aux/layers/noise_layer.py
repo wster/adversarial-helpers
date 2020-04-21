410049155b4545d1e5533b11fe6a26f9b64f0427
August Wester <august.wester@gmail.com>
2020-04-21 13:13:09 +0200

    New revision

--- aux/layers/noise_layer.py
+++ aux/layers/noise_layer.py
@@ -1,25 +1,14 @@
 from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Layer
-from random import randint
 
 class Noise(Layer):
     def __init__(self, minval, maxval, shape, **kwargs):
         self.minval = minval
         self.maxval = maxval
-        self.shape = shape
         super(Noise, self).__init__(**kwargs)
 
-    def build(self, input_shape):
-        shape = list(self.shape)
-        shape.insert(0, 60000)
-        shape = tuple(shape)
-
-        self.noise = K.random_uniform(shape=shape, minval=self.minval, maxval=self.maxval)
-        super(Noise, self).build(input_shape)
-
     def call(self, x):
-        rand_int = randint(0, 60000)
-        noise = self.noise[rand_int]
+        noise = K.random_uniform(shape=K.shape(x), minval=self.minval, maxval=self.maxval)
         return x + noise
 
     def compute_output_shape(self, input_shape):
