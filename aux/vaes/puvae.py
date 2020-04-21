cb71580694e84456e6f25235e59b55448f0e6c17
August Wester <august.wester@gmail.com>
2020-04-21 17:57:53 +0200

    New revision

--- aux/vaes/puvae.py
+++ aux/vaes/puvae.py
@@ -76,9 +76,10 @@ class PuVAE(Model):
         reconstructions = self.decoder([z, y])
 
         print("CALCULATING KL LOSS")
-        #kl_loss = K.square(z_mean) + K.square(z_log_var) - K.log(K.square(z_log_var) - 1)
+
+        kl_loss = K.mean(K.square(z_mean)) + K.mean(K.square(z_log_var)) - K.log(K.mean(K.square(z_log_var)) - 1)
         print("CALCULATED KL LOSS")
-        #self.add_loss(kl_loss)
+        self.add_loss(kl_loss)
         print("DONE CALCULATING KL LOSS")
 
         return (z_mean, z_log_var, reconstructions)
