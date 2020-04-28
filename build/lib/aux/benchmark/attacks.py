d4359f891505b421eadddaf84501540d25867881
August Wester <august.wester@gmail.com>
2020-04-28 02:09:28 +0200

    New revision

--- build/lib/aux/benchmark/attacks.py
+++ build/lib/aux/benchmark/attacks.py
@@ -95,24 +95,11 @@ def basic_iterative_attack(model, images, labels, batch_size=None, epsilons=[0.0
     attack = fa.LinfBasicIterativeAttack()
     return base(attack, model, images, labels, batch_size, epsilons, bounds)
 
-def print_progress(iteration, total, prefix='', suffix='', decimals=1, bar_length=100):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        bar_length  - Optional  : character length of bar (Int)
-    """
-    str_format = "{0:." + str(decimals) + "f}"
-    percents = str_format.format(100 * (iteration / float(total)))
-    filled_length = int(round(bar_length * iteration / float(total)))
-    bar = '█' * filled_length + '-' * (bar_length - filled_length)
-
-    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
-
-    if iteration == total:
-        sys.stdout.write('\n')
-    sys.stdout.flush()
+def show_progress(iteration, total, size=60):
+    file = sys.stdout
+        
+    x = int(size*iteration/total)
+    file.write("%s[%s%s] %i/%i\r" % ("Attacking:", "="*x, "."*(size-x), iteration, total))
+    file.flush()        
+    file.write("\n")
+    file.flush()
