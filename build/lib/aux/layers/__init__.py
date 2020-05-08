a67d9e2eaec98004c5958c1275373e990ccf504c
August Wester <august.wester@gmail.com>
2020-05-08 15:53:04 +0200

    New revision

--- build/lib/aux/layers/__init__.py
+++ build/lib/aux/layers/__init__.py
@@ -1,2 +1,6 @@
 from .noise_layer import Noise
 from .rotate_layer import Rotate
+from .blur import Blur
+from .contrast import Contrast
+from .best_reconstruction import BestReconstruction
+from .prediction import Prediction
