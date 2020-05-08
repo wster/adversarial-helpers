734f449306917c90213819b8658aa59760389070
August Wester <august.wester@gmail.com>
2020-05-08 18:37:24 +0200

    New revision

---
+++ aux/losses/__init__.py
@@ -0,0 +1,2 @@
+from .logit_losses import categorical_crossentropy_with_logits
+from .logit_losses import sparse_categorical_crossentropy_with_logits
---
+++ build/lib/aux/losses/logit_losses.py
@@ -0,0 +1,8 @@
+from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.losses import sparse_categorical_crossentropy
+
+def categorical_crossentropy_with_logits(y_true, y_pred):
+    return categorical_cross_entropy(y_true, y_pred, from_logits=True)
+
+def sparse_categorical_crossentropy_with_logits(y_true, y_pred):
+    return sparse_categorical_crossentropy(y_true, y_pred, from_logits=True)
