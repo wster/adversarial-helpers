0c054c5ecdd6b48b3912c7fd8ef3d27074698559
August Wester <august.wester@gmail.com>
2020-05-11 12:55:51 +0200

    New revision

--- aux/benchmark/attacks.py
+++ aux/benchmark/attacks.py
@@ -73,7 +73,8 @@ def base(attack, model, images, labels, batch_size, epsilons, bounds):
 
     for i, eps in enumerate(epsilons):
         num_successes, num_attacks = outcomes[eps]
-        predicted_advs = np.count_nonzero(success_labels[i] == 10)
+        preds = np.argmax(model(success_imgs[0]), axis=1)
+        predicted_advs = np.count_nonzero(preds == 10)
         print("Predicted {} as adversarial".format(predicted_advs))
 
         print("For epsilon = {}, there were {}/{} successful attacks (robustness = {})".format(eps, num_successes, num_attacks, round(1.0 - num_successes / num_attacks, 3)))
