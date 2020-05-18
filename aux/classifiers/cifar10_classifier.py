09ac68dd52f368529beaab02f99ff9994bbf94e1
August Wester <august.wester@gmail.com>
2020-05-18 11:09:25 +0200

    New revision

--- aux/classifiers/cifar10_classifier.py
+++ aux/classifiers/cifar10_classifier.py
@@ -1,42 +1,21 @@
 from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPool2D, Flatten, BatchNormalization, Activation
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPool2D, Flatten, BatchNormalization, Activation, Dropout
 from tensorflow.keras.regularizers import l2
 
 class CIFAR10Classifier(Model):
     def __init__(self, **kwargs):
         super(CIFAR10Classifier, self).__init__(**kwargs)
 
-        LAMBDA = 1e-4
-        
         self.model = Sequential([
             Input(shape=(32,32,3)),
-            Conv2D(32, (3,3), padding='same', activation='relu', kernel_regularizer=l2(LAMBDA)),
-            BatchNormalization(),
-
-            Conv2D(32, (3,3), padding='same', kernel_regularizer=l2(LAMBDA)),
-            MaxPool2D(pool_size=(2,2)),
-            Activation('relu'),
-            BatchNormalization(),
-
-            Conv2D(64, (3,3), padding='same', activation='relu', kernel_regularizer=l2(LAMBDA)),
-            BatchNormalization(),
-
-            Conv2D(64, (3,3), padding='same', kernel_regularizer=l2(LAMBDA)),
-            MaxPool2D(pool_size=(2,2)),
-            Activation('relu'),
-            BatchNormalization(),
-
-            Conv2D(128, (3,3), padding='same', activation='relu', kernel_regularizer=l2(LAMBDA)),
-            BatchNormalization(),
-            
-            Conv2D(128, (3,3), padding='same', activation='relu', kernel_regularizer=l2(LAMBDA)),
-            MaxPool2D(pool_size=(2,2)),
-            Activation('relu'),
-            BatchNormalization(),
-            Flatten(),
-
-            Dense(32, activation='relu'),
-            Dense(10)
+            Conv2D(64, (5,5), padding='same', activation='relu'),
+            Conv2D(128, (5,5), padding='same', activation='relu'),
+            Conv2D(256, (5,5), padding='same', activation='relu', dilation_rate=2),
+            Dropout(0.25),
+            Dense(512, activation='relu'),
+            Dropout(0.5),
+            Dense(256, activation='relu'),
+            Dense(10, activation='relu'),
         ])
 
     def call(self, inputs):
