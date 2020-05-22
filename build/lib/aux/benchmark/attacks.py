3e0a3262b780de894f662d6592cf18c0e05b3950
August Wester <august.wester@gmail.com>
2020-05-22 17:29:22 +0200

    New revision

--- build/lib/aux/benchmark/attacks.py
+++ build/lib/aux/benchmark/attacks.py
@@ -142,6 +142,7 @@ def base(attack, model, images, labels, batch_size, epsilons, bounds):
 
     for i, eps in enumerate(epsilons):
         num_successes, num_attacks = outcomes[eps]
+        print("Length:", len(success_imgs[i]))
         preds = np.argmax(model.predict(success_imgs[i]), axis=1)
         predicted_advs = np.count_nonzero(preds == 10)
         num_successes -= predicted_advs
